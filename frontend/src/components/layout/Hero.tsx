706323e1eb3bdf2cb1e8fe0650d61fdc115338a4
Max Martin <maxmartin54321@gmail.com>
2025-03-04 14:22:34 -0500

    added typeform

--- frontend/src/components/layout/Hero.tsx
+++ frontend/src/components/layout/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       <div className="min-h-[calc(100vh-60px)] flex flex-col justify-center items-center">
         <h1 className="text-4xl md:text-[6em] text-primary">Designathon</h1>
         <h1 className="text-xl md:text-[2em] mt-[16px] text-secondary">November 7th-8th, 2025</h1>
-        <Button className="my-4 mt-[24px]" variant="default" size="lg">Register Today!</Button>
+        <Button className="my-4 mt-[24px]" variant="default" size="lg" as="a" href="https://form.typeform.com/to/SX2PnmnU">Register Today!</Button>
       </div>
     );
   }
