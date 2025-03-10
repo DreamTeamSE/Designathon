8a9c399a2a04cf34257b26b687ff011b89323286
Max Martin <56135027+maxmartinn@users.noreply.github.com>
2025-03-10 17:35:16 -0400

    Feature  create landing (#5)
    * added bayon font
    
    * remodeled hero

--- frontend/src/components/layout/Hero.tsx
+++ frontend/src/components/layout/Hero.tsx
@@ -1,15 +1,24 @@
-
-import { Button } from "@/components/ui/button"
+import Image from "next/image";
 
 
 export default function Hero() {
     return (
-      <div className="min-h-[calc(100vh-60px)] flex flex-col justify-center items-center">
-        <h1 className="text-4xl md:text-[6em] text-primary">Designathon</h1>
-        <h1 className="text-xl md:text-[2em] mt-[16px] text-secondary">November 7th-8th, 2025</h1>
-        <a href="https://form.typeform.com/to/SX2PnmnU" target="_blank">
-          <Button className="my-4 mt-[24px]" variant="default" size="lg">Register Today!</Button>
-        </a>
+      <div className="pt-[60px] pb-[48px] flex flex-col justify-center items-center">
+        <span className="flex flex-row font-bayon select-none">
+        <h1 className="text-4xl md:text-[6em] relative group">
+          DESIGNATH
+        </h1>
+        <Image
+          src="/assets/transparent_heart.png"
+          alt="X"
+          width={100}
+          height={100}
+          className="object-cover animate-move relative -translate-y-[8px]"
+        />
+        <h1 className="text-4xl md:text-[6em]">N</h1>
+        </span>
+        
+        <h1 className="text-xl md:text-[2em] mt-[8px] text-primary font-bayon">Brought to you by Dream Team Engineering</h1>
       </div>
     );
   }
