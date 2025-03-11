8a8a19d6f6ef4de4506c82ef82bd6ddbec8c6cf6
Max Martin <maxmartin54321@gmail.com>
2025-03-11 18:38:02 -0400

    created home and schedule paths

---
+++ frontend/src/app/schedule/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/layout/Hero";
+import { Separator } from "@/components/ui/separator"
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Separator className="w-[1vw]"/>
+    </div>
+  );
+}
