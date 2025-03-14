2d829a8a2f2d66446b6c84e143cd21918092bc1b
Max Martin <maxmartin54321@gmail.com>
2025-03-14 14:58:14 -0400

    added apply section to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from "@/components/layout/landing_page/Hero";
 import { Separator } from "@/components/ui/items/separator"
+import ApplySection from "@/components/layout/sections/apply-section"
 
-
-export default function Home() {
+export default function Page() {
   return (
     <div>
       <Hero />
       <Separator className="w-[1vw]"/>
+      <ApplySection />
     </div>
   );
 }
