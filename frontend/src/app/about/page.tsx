531323956c7b188ba34119a06b4a3d8d7fe58e67
root <root@DESKTOP-UALISRU.>
2025-04-10 12:26:32 -0400

    finished about us

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,11 +1,17 @@
-import Hero from "@/components/layout/landing_page/Hero";
+import Hero from "@/components/layout/about/Hero";
+import WhatIsDesign from "@/components/layout/about/WhatIsDesign";
 import { Separator } from "@/components/ui/items/separator"
+import FAQs from "@/components/layout/about/FAQs";
 
 
 export default function Page() {
   return (
     <div>
       <Hero />
+      <Separator className="w-[1vw]"/>
+      <WhatIsDesign />
+      <Separator className="w-[1vw]"/>
+      <FAQs />
       <Separator className="w-[1vw]"/>
     </div>
   );
