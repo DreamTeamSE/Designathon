4943867cbcfcb52c38130b4511b137e9386335c8
root <root@DESKTOP-UALISRU.>
2025-04-10 12:38:25 -0400

    added awpply section to FAQs Page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/layout/about/Hero";
 import WhatIsDesign from "@/components/layout/about/WhatIsDesign";
 import { Separator } from "@/components/ui/items/separator"
 import FAQs from "@/components/layout/about/FAQs";
+import ApplySection from "@/components/layout/sections/apply-section";
 
 
 export default function Page() {
@@ -13,6 +14,7 @@ export default function Page() {
       <Separator className="w-[1vw]"/>
       <FAQs />
       <Separator className="w-[1vw]"/>
+      <ApplySection />
     </div>
   );
 }
